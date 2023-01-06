ca6ca8ea4059c6dc115f8e57ccebdca649144284
iradakhf <iradakhf@code.edu.az>
2023-01-06 17:23:04 +0400

    completed index html

--- assets/js/app.js
+++ assets/js/app.js
@@ -91,6 +91,88 @@ $(document).ready(function() {
         // instead of a settings object
       ]
     });
+    $('#mostViewed .productBox').slick({
+      dots: false,
+      infinite: false,
+      prevArrow: false,
+    nextArrow: false,
+      speed: 300,
+      slidesToShow: 5,
+      slidesToScroll: 1,
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 5,
+            slidesToScroll: 1,
+            infinite: false,
+            dots: true
+          }
+        },
+        {
+          breakpoint: 991,
+          settings: {
+            slidesToShow: 4,
+            slidesToScroll: 1
+          }
+        },
+        {
+          breakpoint: 768,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 1
+          }
+        },
+        {
+          breakpoint: 576,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1
+          }
+        }
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+      ]
+    });
+    $('.specialOffer').slick({
+      dots: false,
+      infinite: true,
+      autoplay: true,
+      prevArrow: $('.arrowLeft i'),
+    nextArrow: $('.arrowRight i'),
+      speed: 300,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            infinite: false,
+            dots: true
+          }
+        },
+        {
+          breakpoint: 991,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        },
+        {
+          breakpoint: 768,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+      ]
+    });
     $('.productBoxesSectionSliderForSingleProduct1').slick({
       dots: false,
       infinite: false,
@@ -387,6 +469,7 @@ $(document).ready(function() {
         // instead of a settings object
       ]
     });
+    
    $(".modalBootstrap").modal("show");
    $("a").click(function(e){
       e.preventDefault();
@@ -711,4 +794,43 @@ function on() {
  }
  
  
- 
+ // Set the date we're counting down to
+var countDownDate = new Date("Jan 5, 2024 15:37:25").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+  // Get today's date and time
+  var now = new Date().getTime();
+    
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+    
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    
+  // Output the result
+  document.querySelector(".DAY").innerHTML  = days;
+  document.querySelector(".Hour").innerHTML = hours;
+  document.querySelector(".Min").innerHTML = minutes;
+  document.querySelector(".Sec").innerHTML = seconds;
+  
+  document.querySelector(".DAY2").innerHTML = days;
+  document.querySelector(".Hour2").innerHTML = hours;
+  document.querySelector(".Min2").innerHTML = minutes;
+  document.querySelector(".Sec2").innerHTML = seconds;
+
+  document.querySelector(".DAY3").innerHTML = days;
+  document.querySelector(".Hour3").innerHTML = hours;
+  document.querySelector(".Min3").innerHTML = minutes;
+  document.querySelector(".Sec3").innerHTML = seconds;
+  // If the count down is over, write some text 
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("demo").innerHTML = "EXPIRED";
+  }
+}, 1000);
